b87e2d4c2d25067459a5923f065d51620ba9c0b6
LetzterMensch <letrongnghiavanyen@gmail.com>
2023-09-14 18:00:06 +0700

    init project

---
+++ G4_DailyReport/src/test/java/com/example/G4_DailyReport/G4DailyReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.G4_DailyReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G4DailyReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
