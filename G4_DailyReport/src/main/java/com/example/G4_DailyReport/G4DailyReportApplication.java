3dc588452235ea5ec8da50e5cb98c74f63910a5c
hahunavth <vu.thanh.ha@sun-asterisk.com>
2023-09-17 09:12:35 +0700

    keep working ...

--- G4_DailyReport/src/main/java/com/example/G4_DailyReport/G4DailyReportApplication.java
+++ G4_DailyReport/src/main/java/com/example/G4_DailyReport/G4DailyReportApplication.java
@@ -4,8 +4,10 @@ import com.example.G4_DailyReport.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class G4DailyReportApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(G4DailyReportApplication.class, args);
