baec259fe32cd8ec16a5d8ee60ef68dd4f7ec7ea
hahunavth <vuthanhha.2001@gmail.com>
2023-09-22 16:19:09 +0700

    Add department info and manager list screen

--- G4_DailyReport/src/main/java/com/example/G4_DailyReport/repository/UserRepository.java
+++ G4_DailyReport/src/main/java/com/example/G4_DailyReport/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.G4_DailyReport.repository;
 
 import com.example.G4_DailyReport.model.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Page<User> findAllByDepartmentIdAndPositionName(UUID department, String position, Pageable pageable);
 }
