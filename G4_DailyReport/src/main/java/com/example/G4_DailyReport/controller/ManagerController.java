33f59cc67046a0f93d8fe28fbe4653ed504d2fe4
PhamNhatSang20194657 <sang.pn194657@sis.hust.edu.vn>
2023-09-22 14:58:24 +0700

    Add feature Infor Department Management

--- G4_DailyReport/src/main/java/com/example/G4_DailyReport/controller/ManagerController.java
+++ G4_DailyReport/src/main/java/com/example/G4_DailyReport/controller/ManagerController.java
@@ -4,30 +4,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.G4_DailyReport.repository.UserRepository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.G4_DailyReport.model.User;
+import com.example.G4_DailyReport.service.ManagerService;
 
 @Controller
+@RequestMapping("/manager")
 public class ManagerController {
 	@Autowired
-    private UserRepository userRepository;
-	
-	 @GetMapping("/manager/departments")
-	    public String managerHomepage() {
-	     
-	        return "managers/index";
-	    }
-	 
-	 @GetMapping("/manager/departments/listEmployee")
-	    public String managerDepartments() {
-	     
-	        return "managers/list-employee-in-depart";
-	    }
-	 
-	 @GetMapping("/manager/departments/addEmployee")
-	    public String managerAddEmployee() {
-	     
-	        return "managers/list-employees-toadd";
-	    }
-	 
+	ManagerService managerService;
+
+	@Autowired
+	private ManagerSingleton managerSingleton;
+
+	@ModelAttribute("user")
+	public User loadCurrentUser() {
+		// giả sử đây là manager khi đăng nhập
+		return managerSingleton.getUser();
+	}
+
+	@GetMapping("/departments")
+	public String managerHomepage() {
+		return "managers/index";
+	}
+
+	@GetMapping("/departments/listEmployee")
+	public String managerDepartments(Model model, @RequestParam(defaultValue = "0") int page) {
+		model.addAttribute("usersPage",
+				managerService.getAllUserInDepartment(loadCurrentUser().getDepartment().getId().toString(), page));
+		return "managers/list-employee-in-depart";
+	}
+
+	@GetMapping("/departments/addEmployee")
+	public String managerAddEmployee(Model model, @RequestParam(defaultValue = "0") int page) {
+		model.addAttribute("usersPage", managerService.getAllUserNoDepartment(page));
+		return "managers/list-employees-toadd";
+	}
+
+	@GetMapping("/departments/user")
+	public String addEmployee(String userId) {
+		managerService.getUser(userId).setDepartment(loadCurrentUser().getDepartment());
+		int currentPage = managerService.save(managerService.getUser(userId));
+		return "redirect:/manager/departments/addEmployee?page=" + currentPage;
+	}
+
+	@GetMapping("/departments/delete")
+	public String deleteEmployee(String userId) {
+		int currentPage = managerService.delete(managerService.getUser(userId));
+		return "redirect:/manager/departments/listEmployee?page=" + currentPage;
+	}
+
 }
--- G4_DailyReport/src/main/java/com/example/G4_DailyReport/repository/UserRepository.java
+++ G4_DailyReport/src/main/java/com/example/G4_DailyReport/repository/UserRepository.java
@@ -1,10 +1,20 @@
 package com.example.G4_DailyReport.repository;
 
+import com.example.G4_DailyReport.model.Department;
 import com.example.G4_DailyReport.model.User;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+	Page<User> findByDepartment(Department department, Pageable pageable);
+    Page<User> findByDepartmentIsNull(Pageable pageable);
+    List<User> findByDepartmentIsNull();
+    List<User> findByDepartment(Department department);
+
 }
